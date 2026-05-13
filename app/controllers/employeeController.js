815a39695466952ce70a9a5f57ec1c6869eeeb29
Szász Benedek <benedekszasz7@gmail.com>
2026-05-13 13:59:16 +0200

    kezdés

---
+++ app/controllers/employeeController.js
@@ -0,0 +1,15 @@
+import Employee from "../models/employee.js"
+
+
+const EmployeeController = {
+    getAll: async (req, res) => {
+        const employees = await Employee.findAll()
+        res.json(employees)
+    },
+
+    store: async (req, res) => {
+        const employee = await Employee.create(req.body)
+        res.json(employee)
+    }
+
+}
---
+++ app/database/database.js
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: process.env.DB_STORAGE
+});
+
+
+
+export default sequelize;
